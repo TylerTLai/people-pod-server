e18cbce9cc8025278ebbb4439eb29b0a661e4656
Tyler Lai <lai.t.tyler@gmail.com>
2020-11-30 06:26:19 -0600

    Setup server and connect db

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+dotenv.config();
+
+// Connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PASSWORD,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', () =>
+  console.error.bind(console, 'Failed to connect to MongoDB.')
+);
+db.once('open', () => {
+  console.log('Successfully connected to MongoDB.');
+  app.listen(PORT, (req, res) => console.log(`Server started on ${PORT}.`));
+});
+
+// Init middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Define routes
+
+app.get('/', (req, res) => {
+  res.send('Thanks for using PeoplePod!');
+});
