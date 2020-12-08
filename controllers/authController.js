3d791fd60c4c149916d61cebc834291fdae1e766
Tyler Lai <lai.t.tyler@gmail.com>
2020-12-07 21:31:24 -0600

    Add user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
 
 exports.authorizeUser = async (req, res) => {
@@ -7,5 +11,53 @@ exports.authorizeUser = async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
+  }
+};
+
+exports.validateUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    // Check if user doesn't exists
+    if (!user) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid credentials.' }] });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid credentials.' }] });
+    }
+
+    // Return jwt
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
   }
 };
--- routes/api/authRoute.js
+++ routes/api/authRoute.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
 const auth = require('../../middleware/auth');
 
 const authController = require('../../controllers/authController');
 
 router.get('/', auth, authController.authorizeUser);
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email.').isEmail(),
+    check('password', 'Password is required.').exists(),
+  ],
+  authController.validateUser
+);
 
 module.exports = router;
